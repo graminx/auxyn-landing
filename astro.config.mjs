73b5d528e61ba697c1147e27ba3c1a1c54841e15
Ayush Kumar <kumarayushkumar@protonmail.com>
2026-05-08 08:54:10 +0530

    fix: update site references from Graminx to Auxyn in configuration and layout files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap'
-import tailwindcss from '@tailwindcss/vite';
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://graminx.com/',
+  site: "https://auxyn.in/",
   integrations: [sitemap()],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
